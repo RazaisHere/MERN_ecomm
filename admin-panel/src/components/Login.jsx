34514732ede5f62975f4b9065e7f4aa996cc8c44
Ali Raza <info678797@gmail.com>
2024-10-29 16:43:52 +0500

    Update Login.jsx

--- admin-panel/src/components/Login.jsx
+++ admin-panel/src/components/Login.jsx
@@ -36,7 +36,7 @@ export default function Login({ setToken }) {
                     <div className="mb-3 min-w-72">
                         <p className="text-sm font-medium text-gray-700 mb-2">Email address</p>
                         <input
-                            placeholder="admin321@gmail.com"
+                            placeholder="Enter Email"
                             onChange={(e) => setAdminEmail(e.target.value)}
                             className="rounded-md w-full px-3 py-2 border border-gray-300 outline-none"
                             type="email"
@@ -45,7 +45,7 @@ export default function Login({ setToken }) {
                     <div className="mb-3 min-w-72">
                         <p className="text-sm font-medium text-gray-700 mb-2">Password</p>
                         <input
-                            placeholder="admin321"
+                            placeholder="Enter Password"
                             onChange={(e) => setAdminPassword(e.target.value)}
                             className="rounded-md w-full px-3 py-2 border border-gray-300 outline-none"
                             type="password"
